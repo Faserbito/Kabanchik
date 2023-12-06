69c85b2592de2db1b1890cb77fcb098491d09433
Faserbito <147964334+Faserbito@users.noreply.github.com>
2023-12-06 09:17:51 +0500

    Add files via upload

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Олег\Desktop\Женя\Институт\4 курс\2 семестр\Алгоритмы шифрования информации\! Курсовая работа\DjangoWebProject\DjangoWebProject\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
